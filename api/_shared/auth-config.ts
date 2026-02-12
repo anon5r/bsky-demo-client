d1c59548a897ee8fb808cb1fd72742b0bc4194b1
anon <66147+anon5r@users.noreply.github.com>
2026-02-12 20:24:38 +0900

    refactor: move shared config to api/_shared/ for Vercel compatibility

---
+++ api/_shared/auth-config.ts
@@ -0,0 +1,8 @@
+export const OAUTH_SCOPE = [
+  'atproto',
+  'include:app.bsky.authFullApp?aud=api.bsky.app#bsky_appview',
+  'include:app.chronosky.authClient?aud=chronosky.app#chronosky_xrpc',
+  'blob:image/*',
+  'blob:video/*',
+];
+export const CLIENT_NAME = "Bluesky Client Demo App";
