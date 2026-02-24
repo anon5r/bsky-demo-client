5dcd106e375414251b4f903256ffab63006c0fb4
anon <66147+anon5r@users.noreply.github.com>
2026-02-24 12:40:49 +0900

    refactor: uncomment Chronosky auth scope with audience in OAuth config

--- api/_shared/auth-config.ts
+++ api/_shared/auth-config.ts
@@ -1,8 +1,8 @@
 export const OAUTH_SCOPE = [
   'atproto',
   'include:app.bsky.authFullApp?aud=did:web:api.bsky.app%23bsky_appview',
-  'include:app.chronosky.authClient',
-  // 'include:app.chronosky.authClient?aud=did:web:api.chronosky.app%23chronosky_xrpc',
+  // 'include:app.chronosky.authClient',
+  'include:app.chronosky.authClient?aud=did:web:api.chronosky.app%23chronosky_xrpc',
   'blob:image/*',
   'blob:video/*',
 ];
